c324d9dfdc75c94c4001c3089bfa5c60fa7c144a
abs-cbn-tf <133660640+abs-cbn-tf@users.noreply.github.com>
2023-05-31 16:13:46 +0530

    Delete kms directory

--- kms/modules/kms/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "kms_keys" {
-  type = list(object({
-    name            = string
-    description     = string
-    deletion_window = number
-  }))
-}
-
--- kms/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "kms_key_arns" {
-  value = module.kms.kms_key_arns
-}
-
