991c51c90b758bd038a5ab77ef7b111f8fb8568e
abs-cbn-tf <133660640+abs-cbn-tf@users.noreply.github.com>
2023-05-31 16:13:36 +0530

    Delete iam directory

--- iam/abs-iamuserpolicy/output.tf
+++
@@ -1,8 +0,0 @@
-output "iam_user_name" {
-  value = module.iam_user.iam_user_name
-}
-
-#output "iam_policy_arns" {
-#  value = module.iam.iam_policy_arns
-#}
-
--- iam/abs-iamuserpolicy/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "us-west-2"  # Adjust the region according to your requirements
-  profile = "default"
-}
-
