0162fcc585ceebeb7b0fea117d08aa9782d58687
Ronel Migs Perez <rjperez@abs-cbn.com>
2023-06-14 13:29:48 +0800

    Migs - added module to create apigw alone

--- ManagedServices/apigw/apigw.tf
+++ ManagedServices/apigw/apigw.tf
@@ -1,5 +1,5 @@
-module "apigw" {
-  source        = "./modules" # Mandatory
+module "apigw_lambda" {
+  source        = "./modules/apigw-lambda" # Mandatory
   aws_region    = var.aws_region
   apigw_name    = var.apigw_name
   resource_name = var.resource_name
@@ -7,5 +7,15 @@ module "apigw" {
   stage_name    = var.stage_name
   api_key       = var.api_key
   usage_plan    = var.usage_plan
+}
 
+module "apigw" {
+  source        = "./modules/apigw" # Mandatory
+  aws_region    = var.aws_region
+  apigw_name    = var.apigw_name
+  resource_name = var.resource_name
+  method_name   = var.method_name
+  stage_name    = var.stage_name
+  api_key       = var.api_key
+  usage_plan    = var.usage_plan
 }
--- ManagedServices/apigw/output.tf
+++ ManagedServices/apigw/output.tf
@@ -1,4 +1,8 @@
 # Output the API Gateway endpoint
-output "api_gateway_endpoint" {
-  value = module.apigw.api_gateway_endpoint
+output "apigw_lambda_gateway_endpoint" {
+  value = module.apigw_lambda.api_gateway_endpoint
 }
+# output "apigw_gateway_endpoint" {
+#   value = module.apigw.api_gateway_endpoint
+# }
+
